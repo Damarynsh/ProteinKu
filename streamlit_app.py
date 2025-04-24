34b74f356d2f8243f43e62af67ef981b05caeb62
Damarynsh <damaryansyah2@gmail.com>
2025-04-24 18:51:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("HAIII SOBAT :red[PANGAN]")
-st.write("Yuk Cari Tahu Berapa Banyak Sih Kebutuhan Protein harian kamu?")
+st.write("Yuk Cari Tahu Berapa Banyak Sih Kebutuhan _Protein_ harian kamu? :thinking:")
