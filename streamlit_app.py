7442558aa0af10f2ff78494dc02754e665d3a09d
Damarynsh <damaryansyah2@gmail.com>
2025-04-24 21:44:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
-# CSS untuk mengganti background Streamlit
+# CSS: background gambar + overlay gelap transparan
 page_bg_img = '''
 <style>
 [data-testid="stAppViewContainer"] {
-    background-image: url("https://img.freepik.com/free-photo/top-view-healthy-food-high-protein_23-2148761359.jpg");
+    background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+                      url("https://img.freepik.com/free-photo/top-view-healthy-food-high-protein_23-2148761359.jpg");
     background-size: cover;
-    background-repeat: no-repeat;
+    background-position: center top;
     background-attachment: fixed;
+    background-repeat: no-repeat;
 }
 </style>
 '''
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Judul dengan teks 'PROTEIN' berwarna merah
+# Judul dengan teks 'PROTEIN' warna merah
 st.markdown(
     "<h1 style='text-align: center; color:white;'>BERAPA SIH KEBUTUHAN <span style='color: red;'>PROTEIN</span> HARIAN MU???</h1>",
     unsafe_allow_html=True
