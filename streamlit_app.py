ced5405e2bc911812c6d359c436f0ac7d77960b6
Damarynsh <damaryansyah2@gmail.com>
2025-04-24 21:07:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>BERAPA SIH KEBUTUHAN <span style='color: red;'>PROTEIN</span> HARIAN MU???</h1>",
     unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: red;'> Halloo Sobat Pangan!!👋 </h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>👋Halloo Sobat Pangan!!👋 </h1>", unsafe_allow_html=True)
 
 st.write("Yuk Cari Tahu Berapa Banyak Sih Kebutuhan _Protein_ harian kamu? 🤔")
