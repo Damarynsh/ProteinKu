534926c5e0168816bf45a87b0db64117b7d69426
Damarynsh <damaryansyah2@gmail.com>
2025-04-24 21:35:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>BERAPA SIH KEBUTUHAN <span style='color: red;'>PROTEIN</span> HARIAN MU???</h1>",
-    unsafe_allow_html=True)
+# Gambar background dari URL (nomor 4 - variasi makanan tinggi protein)
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://img.freepik.com/free-photo/top-view-healthy-food-high-protein_23-2148761359.jpg");
+background-size: cover;
+background-attachment: fixed;
+background-repeat: no-repeat;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Judul dengan teks 'PROTEIN' berwarna merah
+st.markdown(
+    "<h1 style='text-align: center; color:white;'>BERAPA SIH KEBUTUHAN <span style='color: red;'>PROTEIN</span> HARIAN MU???</h1>",
+    unsafe_allow_html=True
+)
+
 st.markdown("<h1 style='text-align: center; color: red;'>👋Halloo Sobat Pangan👋 </h1>", unsafe_allow_html=True)
 
 st.write("Yuk Cari Tahu Berapa Banyak Sih Kebutuhan _Protein_ harian kamu? 🤔")
