bbc24f18fdf8354b3b7ccfdddc752ea5b9af7d3c
Damarynsh <damaryansyah2@gmail.com>
2025-05-16 11:07:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.markdown(
 
     div.stButton > button {
         color: white;
-        background-color: #000000;
+        background-color: #ffffff;
         border: none;
         border-radius: 12px;
         padding: 0.75em 2em;
